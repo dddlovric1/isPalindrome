4a7f22580591d11f70f01cf7bbf248fc961e66fe
dddlovric <dddlovric@gmail.com>
2019-04-03 15:48:47 +0200

    HarmlessRansomNote

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,9 +1,9 @@
 function fizzBuzz(num) {
     for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz')
+        if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz');
         else if  (i % 3 === 0) console.log('Fizz');
         else if (i % 5 === 0) console.log('Buzz');
-        else console.log(i); //ako nije djeljiv samo ispiši broj
+        else console.log(i);                                    //ako nije djeljiv samo ispiši broj
     }
 }
 
